919cc4623630ddee110935bab87eb3fe2008f0cc
Hehoku <hehoku@outlook.com>
2021-04-12 22:53:30 +0800

    add tailwindcss and remove reportWebVitals for can't compile it

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FunctionComponent = () => {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
+        <p className='text-white-300 text-lg'>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
